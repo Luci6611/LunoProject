f0bf51fa4dc6af3fff6d34ff6ce2315bd05906e6
luciano rivera <lucianonicolasrivera8@gmail.com>
2023-02-28 08:34:48 -0300

    inicio de proyecto con la base

---
+++ script.js
@@ -0,0 +1,10 @@
+const botonNav = document.getElementById("boton-nav")
+const nav = document.getElementById("navegador")
+
+botonNav.addEventListener("click",()=>{
+    nav.classList.toggle("nav__conteiner-abrir")
+})
+
+botonNav.addEventListener("click",()=>{
+    botonNav.classList.toggle("nav__animacion")
+})
